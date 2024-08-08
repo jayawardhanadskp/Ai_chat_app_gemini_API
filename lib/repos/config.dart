f1a2f4a112c0f5f3fd6ffb403ea79e0d704d977f
kasun <kasunpradeep.ds@gmail.com>
2024-08-08 11:43:22 +0530

    config

--- lib/repos/config.dart
+++ lib/repos/config.dart
@@ -1,5 +1,5 @@
 class Config {
   // ignore: constant_identifier_names
   static const BACKEND_API_KEY =
-      String.fromEnvironment('AIzaSyDtUOg0un6-cIqg3W1jhGEYfx40ojMU2Iw');
+      String.fromEnvironment('');
 }
