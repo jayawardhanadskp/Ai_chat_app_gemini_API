9e6921edd6ea19ca7e648f75e9faa0923e5f3f4e
kasun <kasunpradeep.ds@gmail.com>
2024-08-08 11:41:14 +0530

    main

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:ai_app/pages/home_page.dart';
+import 'package:ai_app/theme/theme.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: ThemeMode.system,
+      home: HomePage(),
+    );
+  }
+}
