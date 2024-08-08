f42dd75f339befe7ff722bfb6778874f3f107cb4
kasun <kasunpradeep.ds@gmail.com>
2024-08-08 11:40:55 +0530

    chat repo

---
+++ lib/repos/chat_repo.dart
@@ -0,0 +1,37 @@
+
+import 'package:dio/dio.dart';
+import 'package:logger/logger.dart'; 
+
+import '../models/message_model.dart';
+
+class ChatRepo {
+  static final Logger _logger = Logger(); 
+
+  static Future<String> chatTextGenerationRepo(
+      List<ChatMessageModel> previousMessages) async {
+    try {
+      Dio dio = Dio();
+      final response = await dio.post(
+          'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyDtUOg0un6-cIqg3W1jhGEYfx40ojMU2Iw',
+          data: {
+            "contents": previousMessages.map((e) => e.toMap()).toList(),
+            "generationConfig": {
+              "temperature": 2,
+              "topK": 64,
+              "topP": 0.95,
+              "maxOutputTokens": 8192,
+              "responseMimeType": "text/plain"
+            }
+          });
+      _logger.i(response.toString());
+      if (response.statusCode == 200) {
+        return response
+            .data['candidates'].first['content']['parts'].first['text'];
+      }
+      return '';
+    } catch (e) {
+      _logger.e('error: ${e.toString()}');
+      return '';
+    }
+  }
+}
